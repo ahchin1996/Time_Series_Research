49f14bd2ae32f597e5406a7e40b5253eef63096b
ahchin1996 <cooker24624872@gmail.com>
2021-04-26 13:14:42 +0800

    update all files

--- GRU code/GRU_recover_best_model.py
+++ GRU code/GRU_recover_best_model.py
@@ -43,10 +43,10 @@ def print_time(text, stime):
     print(text +" "+ str(seconds // 60 // 60)+" hours : " + str(seconds // 60 % 60)  + " minutes : " + str(np.round(seconds % 60)) + " seconds")
 
 #每次需更改項目
-year = 2019
-fd = 'N225_2019'
-path =  'D:/Time_Series_Research/new_data/N225/N225_2019.csv'
-repot_path = 'D:/Time_Series_Research/LSTM code/LSTM_parameter_result/N225_2019_p.csv'
+year = 2018
+fd = 'HSI_2018'
+path =  'D:/Time_Series_Research/new_data/HSI/HSI_2018.csv'
+repot_path = 'D:/Time_Series_Research/LSTM code/LSTM_parameter_result/042521212618.csv'
 
 INPUT_PATH = os.path.join(path, "inputs")
 
